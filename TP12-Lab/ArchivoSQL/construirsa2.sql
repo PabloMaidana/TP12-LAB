f2cd1c3f34c3c2b0f4751752b867d4d01c77116c
PabloMaidana <pablomaid1@gmail.com>
2024-05-10 18:03:47 -0300

    proyecto

---
+++ TP12-Lab/ArchivoSQL/construirsa2.sql
@@ -0,0 +1,146 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 10-05-2024 a las 22:58:46
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `construirsa2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `empleado`
+--
+
+CREATE TABLE `empleado` (
+  `id_empleado` int(11) NOT NULL,
+  `dni` bigint(20) NOT NULL,
+  `apellido` varchar(58) NOT NULL,
+  `nombre` varchar(58) NOT NULL,
+  `acceso` int(11) NOT NULL,
+  `estado` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `empleado`
+--
+
+INSERT INTO `empleado` (`id_empleado`, `dni`, `apellido`, `nombre`, `acceso`, `estado`) VALUES
+(1, 111, 'Martinez', 'Gonzalo', 1, 0),
+(2, 222, 'Perez', 'Camila', 1, 1),
+(3, 333, 'Romano', 'Carla', 1, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `herramienta`
+--
+
+CREATE TABLE `herramienta` (
+  `id_herramienta` int(11) NOT NULL,
+  `nombre` varchar(60) NOT NULL,
+  `descripcion` varchar(100) NOT NULL,
+  `stock` int(11) NOT NULL,
+  `estado` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `herramienta`
+--
+
+INSERT INTO `herramienta` (`id_herramienta`, `nombre`, `descripcion`, `stock`, `estado`) VALUES
+(1, 'Martillo', 'Stanley', 12, 1),
+(2, 'Pinza', 'Stanley', 10, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `movimiento`
+--
+
+CREATE TABLE `movimiento` (
+  `id_movimiento` int(11) NOT NULL,
+  `id_empleado` int(11) NOT NULL,
+  `id_herramienta` int(11) NOT NULL,
+  `fechap` date NOT NULL,
+  `fechad` date NOT NULL,
+  `cantidadret` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `empleado`
+--
+ALTER TABLE `empleado`
+  ADD PRIMARY KEY (`id_empleado`),
+  ADD UNIQUE KEY `dni` (`dni`);
+
+--
+-- Indices de la tabla `herramienta`
+--
+ALTER TABLE `herramienta`
+  ADD PRIMARY KEY (`id_herramienta`);
+
+--
+-- Indices de la tabla `movimiento`
+--
+ALTER TABLE `movimiento`
+  ADD PRIMARY KEY (`id_movimiento`),
+  ADD KEY `id_empleado` (`id_empleado`),
+  ADD KEY `id_herramienta` (`id_herramienta`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `empleado`
+--
+ALTER TABLE `empleado`
+  MODIFY `id_empleado` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT de la tabla `herramienta`
+--
+ALTER TABLE `herramienta`
+  MODIFY `id_herramienta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `movimiento`
+--
+ALTER TABLE `movimiento`
+  MODIFY `id_movimiento` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `movimiento`
+--
+ALTER TABLE `movimiento`
+  ADD CONSTRAINT `movimiento_ibfk_1` FOREIGN KEY (`id_empleado`) REFERENCES `empleado` (`id_empleado`),
+  ADD CONSTRAINT `movimiento_ibfk_2` FOREIGN KEY (`id_herramienta`) REFERENCES `herramienta` (`id_herramienta`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ TP12-Lab/src/tp12/lab/TP12Lab.java
@@ -0,0 +1,111 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp12.lab;
+
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author pablo
+ */
+public class TP12Lab {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try{
+        // Establezco el driver
+           Class.forName("org.mariadb.jdbc.Driver");
+           String bd = "jdbc:mysql://localhost/construirsa2";
+           String usuario = "root";
+           String password = "";
+           
+        // Realizo la conexión con la base de datos 
+           Connection con = DriverManager.getConnection(bd,usuario,password);
+           PreparedStatement ps;
+           
+        // Inserto los empleados mediante un metodo
+           
+           insertarEmpleadoDB("111","Martinez","Gonzalo","1","1",con);
+           insertarEmpleadoDB("222","Perez","Camila","1","1",con);
+           insertarEmpleadoDB("333","Romano","Carla","1","0",con);
+        
+        // Inserto las herramientas mediante otro metodo
+    
+           insertarHerramientaDB("Martillo","Stanley","12","1",con);
+           insertarHerramientaDB("Pinza","Stanley","10","1",con);
+            
+
+        // Listo todas las herramientas stock > 10
+            int stock = 10;
+        // Preparo la consulta con stock valor dinamico y se lo asigno posteriormente con setInt
+            ps = con.prepareStatement("SELECT * FROM `herramienta` WHERE stock > ?");
+            ps.setInt(1, stock);
+        // Ejecuto la consulta con executeQuery por que es una sentencia SELECT
+            ResultSet datos = ps.executeQuery();
+
+            System.out.println("Herramientas con stock mayor a 10");
+        // Mientras exista una fila siguiente
+            while (datos.next()) {
+                System.out.println("Nombre: " + datos.getString("nombre"));
+                System.out.println("Descripcion: " + datos.getString("descripcion"));
+                System.out.println("Stock: " + datos.getInt("stock"));
+            }
+        
+        // Doy de baja el primer empleado o empleado con id 1
+        ps = con.prepareStatement("UPDATE `empleado` SET `estado`= 0 WHERE id_empleado = 1");
+        ps.executeUpdate(); 
+           
+        }catch (ClassNotFoundException ex) {
+            JOptionPane.showMessageDialog(null, "Error al cargar el driver");
+        } catch (SQLException ex) {
+            int codigoE=ex.getErrorCode();
+            if(codigoE==1062){
+                  JOptionPane.showMessageDialog(null, "Entrada Duplicada");
+            } else if(codigoE==1049){
+            JOptionPane.showMessageDialog(null, "BD Desconocida");
+            }else{
+                  JOptionPane.showMessageDialog(null, "Error ");
+            }
+            
+            ex.printStackTrace();
+            System.out.println("codigo de error "+ex.getErrorCode());
+        }
+    }
+    
+    public static void insertarEmpleadoDB(String dni,String apellido,String nombre,String acceso,String estado,Connection con) throws SQLException{
+        // Indico que el metodo puede tirar una excepcion SQLException y que la maneje el main
+        // Establezco como va a ser la consulta con los datos recibidos por parametro
+        String sql = "INSERT INTO "
+                   + "empleado(dni, apellido, nombre, acceso, estado) "
+                   + "VALUES ("+ dni +",'"+apellido+"','"+nombre+"',"+acceso+","+estado+")"; 
+        
+        // Preparo la consulta
+        PreparedStatement ps = con.prepareStatement(sql);
+        
+        // La ejecuto con executeUpdate por que es un INSERT
+        int filas = ps.executeUpdate();
+        
+        if (filas > 0) {
+            System.out.println("Se ha agregado el alumno: " + nombre + " " + apellido);
+        }
+    }
+    
+    public static void insertarHerramientaDB(String nombre, String descripcion, String stock, String estado,Connection con) throws SQLException{ 
+        // IDEM metodo insertarEmpleado
+        String sql = "INSERT INTO "
+                   + "herramienta(nombre, descripcion, stock, estado) "
+                   + "VALUES ('"+nombre+"','"+descripcion+"',"+stock+","+estado+")"; 
+        
+        PreparedStatement ps = con.prepareStatement(sql);
+        int filas = ps.executeUpdate();
+        if (filas > 0) {
+            System.out.println("Se ha agregado la herramienta: " + nombre);
+        }
+    }
+ 
+}
